0d274d4dff98bf53bba2e5564fc389637c3e1888
Navarro <damsell@f018985620e3.ant.amazon.com>
2021-04-17 15:33:46 +0800

    Updated files

--- supplemental/initial-commit/index.py
+++ supplemental/initial-commit/index.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
+
+import boto3
+
+ssm = boto3.client('ssm', region_name='us-east-2')
+
+response = ssm.get_parameters(
+    Names=['my-access-key'],
+    WithDecryption=True
+)
+
+print(response['Parameters'][0]['Value'])
+
+# access_key = "AKIAZELJK6BJF5234HAC"
+
 @app.route("/")
 def hello():
     return "Hello World!"
